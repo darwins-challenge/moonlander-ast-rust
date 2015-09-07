ddab2cdcf51fd430734266e0b9960349f6e32f70
dvberkel <daan.v.berkel.1980@gmail.com>
2015-09-07 14:59:02 +0200

    Introduce a Evaluate Trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
+pub trait Evaluate {
+	fn evaluate(&self) -> Command;
+}
+
 pub enum Program {
 	Command(Command),
+}
+
+impl Evaluate for Program {
+	fn evaluate(&self) -> Command {
+		Command::Skip
+	}
 }
 
 pub enum Command {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 extern crate ast;
 
+use ast::Evaluate;
+
 fn main() {
 	let program: ast::Program = ast::Program::Command(ast::Command::Skip);
 
-	match program {
-		ast::Program::Command(_) => println!("Found a command")
-	}
+	let command = program.evaluate();
+
+	let message = match command {
+		ast::Command::Skip  => "skip",
+		ast::Command::Left  => "turnLeft",
+		ast::Command::Right => "turnRight",
+		ast::Command::Up    => "thruster",
+	};
+
+	println!("action is {}", message);
 }
