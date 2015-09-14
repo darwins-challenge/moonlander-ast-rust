67124aa3da221f0266dac76dd37d56ffe85701e8
dvberkel <daan.v.berkel.1980@gmail.com>
2015-09-14 15:53:00 +0200

    Create test to check behavior of source

--- src/source.rs
+++ src/source.rs
@@ -67,3 +67,61 @@ impl Source for Command {
 		}
 	}
 }
+
+#[test]
+fn should_correctly_source_programs() {
+	assert_eq!("iff!(T!(),skip!(),left!())", 
+		Program::If(Condition::True, Box::new(Program::Command(Command::Skip)), Box::new(Program::Command(Command::Left))).source());
+	assert_eq!("skip!()", Program::Command(Command::Skip).source());
+}
+
+#[test]
+fn should_correctly_source_conditions() {
+	assert_eq!("T!()", Condition::True.source());
+	assert_eq!("F!()", Condition::False.source());
+	assert_eq!("not!(T!())", Condition::Not(Box::new(Condition::True)).source());
+	assert_eq!("or!(T!(),F!())", Condition::Or(Box::new(Condition::True), Box::new(Condition::False)).source());
+	assert_eq!("and!(F!(),T!())", Condition::And(Box::new(Condition::False), Box::new(Condition::True)).source());
+	assert_eq!("less!(constant!(1.0000),constant!(2.0000))", 
+		Condition::Less(Box::new(Expression::Constant(1.0)), Box::new(Expression::Constant(2.0))).source());
+	assert_eq!("less_equal!(constant!(1.0000),constant!(2.0000))", 
+		Condition::LessEqual(Box::new(Expression::Constant(1.0)), Box::new(Expression::Constant(2.0))).source());
+	assert_eq!("equal!(constant!(1.0000),constant!(2.0000))", 
+		Condition::Equal(Box::new(Expression::Constant(1.0)), Box::new(Expression::Constant(2.0))).source());
+	assert_eq!("greater_equal!(constant!(1.0000),constant!(2.0000))", 
+		Condition::GreaterEqual(Box::new(Expression::Constant(1.0)), Box::new(Expression::Constant(2.0))).source());
+	assert_eq!("greater!(constant!(1.0000),constant!(2.0000))", 
+		Condition::Greater(Box::new(Expression::Constant(1.0)), Box::new(Expression::Constant(2.0))).source());
+}
+
+#[test]
+fn should_correctly_source_expressions() {
+	assert_eq!("constant!(1.0000)", Expression::Constant(1.0).source());
+	assert_eq!("vx!()", Expression::Sensor(Sensor::Vx).source());
+	assert_eq!("plus!(constant!(1.0000),vy!())", 
+		Expression::Plus(Box::new(Expression::Constant(1.0)),Box::new(Expression::Sensor(Sensor::Vy))).source());
+	assert_eq!("minus!(constant!(1.0000),constant!(2.0000))", 
+		Expression::Minus(Box::new(Expression::Constant(1.0)),Box::new(Expression::Constant(2.0))).source());
+	assert_eq!("multiply!(constant!(1.0000),constant!(2.0000))", 
+		Expression::Multiply(Box::new(Expression::Constant(1.0)),Box::new(Expression::Constant(2.0))).source());
+	assert_eq!("divide!(constant!(1.0000),constant!(2.0000))", 
+		Expression::Divide(Box::new(Expression::Constant(1.0)),Box::new(Expression::Constant(2.0))).source());
+}
+
+#[test]
+fn should_correctly_source_sensors() {
+	assert_eq!("x!()",  Sensor::X.source());
+	assert_eq!("u!()",  Sensor::Y.source());
+	assert_eq!("vx!()", Sensor::Vx.source());
+	assert_eq!("vy!()", Sensor::Vy.source());	
+	assert_eq!("o!()",  Sensor::O.source());	
+	assert_eq!("w!()",  Sensor::W.source());	
+}
+
+#[test]
+fn should_correctly_source_commands() {
+	assert_eq!("skip!()",  Command::Skip.source());
+	assert_eq!("left!()",  Command::Left.source());
+	assert_eq!("right!()", Command::Right.source());
+	assert_eq!("up!()",    Command::Up.source());
+}
