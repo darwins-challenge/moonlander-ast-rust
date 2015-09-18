32da4d67aa3ddb56ad414ee8470958b77ff3fb4d
dvberkel <daan.v.berkel.1980@gmail.com>
2015-09-18 17:37:39 +0200

    Prepare for weighted random AST generation

--- src/random.rs
+++ src/random.rs
@@ -5,8 +5,8 @@ use super::structure::{Program,Condition,Command,Expression,Sensor};
 impl rand::Rand for Program {
 	fn rand<R: rand::Rng>(rng: &mut R) -> Self {
 		match rng.gen_range(0, 2) {
-			0 => Program::If(Condition::rand(rng), Box::new(Program::rand(rng)), Box::new(Program::rand(rng))),
-			1 => Program::Command(Command::rand(rng)),
+			i @ 0...1 if i < 1 => Program::If(Condition::rand(rng), Box::new(Program::rand(rng)), Box::new(Program::rand(rng))),
+			i @ 1...2 if i < 2 => Program::Command(Command::rand(rng)),
 			_ => panic!(),
 		}
 	}
@@ -15,16 +15,16 @@ impl rand::Rand for Program {
 impl rand::Rand for Condition {
 	fn rand<R: rand::Rng>(rng: &mut R) -> Self {
 		match rng.gen_range(0, 10) {
-			0 => Condition::True,
-			1 => Condition::False,
-			2 => Condition::Not(Box::new(Condition::rand(rng))),
-			3 => Condition::Or(Box::new(Condition::rand(rng)), Box::new(Condition::rand(rng))),
-			4 => Condition::And(Box::new(Condition::rand(rng)), Box::new(Condition::rand(rng))),
-			5 => Condition::Less(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
-			6 => Condition::LessEqual(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
-			7 => Condition::Equal(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
-			8 => Condition::GreaterEqual(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
-			9 => Condition::Greater(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
+			i @ 0...1  if i < 1  => Condition::True,
+			i @ 1...2  if i < 2  => Condition::False,
+			i @ 2...3  if i < 3  => Condition::Not(Box::new(Condition::rand(rng))),
+			i @ 3...4  if i < 4  => Condition::Or(Box::new(Condition::rand(rng)), Box::new(Condition::rand(rng))),
+			i @ 4...5  if i < 5  => Condition::And(Box::new(Condition::rand(rng)), Box::new(Condition::rand(rng))),
+			i @ 5...6  if i < 6  => Condition::Less(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
+			i @ 6...7  if i < 7  => Condition::LessEqual(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
+			i @ 7...8  if i < 8  => Condition::Equal(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
+			i @ 8...9  if i < 9  => Condition::GreaterEqual(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
+			i @ 9...10 if i < 10 => Condition::Greater(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
 			_ => panic!(),
 		}
 	}
@@ -33,10 +33,10 @@ impl rand::Rand for Condition {
 impl rand::Rand for Command {
 	fn rand<R: rand::Rng>(rng: &mut R) -> Self {
 		match rng.gen_range(0, 4) {
-			0 => Command::Skip,
-			1 => Command::Left,
-			2 => Command::Right,
-			3 => Command::Up,
+			i @ 0...1 if i < 1 => Command::Skip,
+			i @ 1...2 if i < 2 => Command::Left,
+			i @ 2...3 if i < 3 => Command::Right,
+			i @ 3...4 if i < 4 => Command::Up,
 			_ => panic!(),
 		}
 	}
@@ -45,12 +45,12 @@ impl rand::Rand for Command {
 impl rand::Rand for Expression {
 	fn rand<R: rand::Rng>(rng: &mut R) -> Self {
 		match rng.gen_range(0, 6) {
-			0 => Expression::Constant(rng.next_f32()),
-			1 => Expression::Sensor(Sensor::rand(rng)),
-			2 => Expression::Plus(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
-			3 => Expression::Minus(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
-			4 => Expression::Multiply(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
-			5 => Expression::Divide(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
+			i @ 0...1 if i < 1 => Expression::Constant(rng.next_f32()),
+			i @ 1...2 if i < 2 => Expression::Sensor(Sensor::rand(rng)),
+			i @ 2...3 if i < 3 => Expression::Plus(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
+			i @ 3...4 if i < 4 => Expression::Minus(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
+			i @ 4...5 if i < 5 => Expression::Multiply(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
+			i @ 5...6 if i < 6 => Expression::Divide(Box::new(Expression::rand(rng)), Box::new(Expression::rand(rng))),
 			_ => panic!(),
 		}
 	}
@@ -59,12 +59,12 @@ impl rand::Rand for Expression {
 impl rand::Rand for Sensor {
 	fn rand<R: rand::Rng>(rng: &mut R) -> Self {
 		match rng.gen_range(0, 6) {
-			0 => Sensor::X,
-			1 => Sensor::Y,
-			2 => Sensor::Vx,
-			3 => Sensor::Vy,
-			4 => Sensor::O,
-			5 => Sensor::W,
+			i @ 0...1 if i < 1 => Sensor::X,
+			i @ 1...2 if i < 2 => Sensor::Y,
+			i @ 2...3 if i < 3 => Sensor::Vx,
+			i @ 3...4 if i < 4 => Sensor::Vy,
+			i @ 4...5 if i < 5 => Sensor::O,
+			i @ 5...6 if i < 6 => Sensor::W,
 			_ => panic!(),
 		}
 	}
