8fd1eae44676b2756fddee72fa5639125c5e10d9
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-03-15 21:10:18 +0100

    Module documentation for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+//! This crate explores [genetic
+//! programming](https://en.wikipedia.org/wiki/Genetic_programming). Genetic
+//! programming is
+//!
+//! > a technique whereby computer programs are encoded as a set of genes that
+//! > are then modified (evolved) using an evolutionary algorithm.
+//!
+//! The problem we are trying to tackle with genetic programming is that of
+//! landing a [moon lander](https://en.wikipedia.org/wiki/Lunar_lander). I.e.
+//! land a moon lander safely on the surface of a moon without user
+//! intervention. A nice interactive game that gives a feel for the problem can be
+//! found [here](http://moonlander.seb.ly/).
+
 extern crate rustc_serialize;
 pub mod macros;
 pub mod structure;
