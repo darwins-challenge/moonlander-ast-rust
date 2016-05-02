b22625cd3ce30ecc2728cdef6ceb0e07e93d92ff
Rico Huijbers <rix0rrr@gmail.com>
2016-05-02 22:39:15 +0200

    Get rid of useless type annotations in crossover

--- src/darwin/crossover.rs
+++ src/darwin/crossover.rs
@@ -32,35 +32,35 @@ pub fn cross_over<H: rand::Rng + Sized>(a: &Program, b: &Program, rng: &mut H) -
     let picked_type = choose(nodes, rng);
 
     match picked_type {
-        NodeType::Program    => cross_over_program(a, b, choose(a_nodes.programs, rng), choose(b_nodes.programs, rng), rng),
-        NodeType::Expression => cross_over_expression(a, b, choose(a_nodes.expressions, rng), choose(b_nodes.expressions, rng), rng),
-        NodeType::Condition  => cross_over_condition(a, b, choose(a_nodes.conditions, rng), choose(b_nodes.conditions, rng), rng),
-        NodeType::Sensor     => cross_over_sensor(a, b, choose(a_nodes.sensors, rng), choose(b_nodes.sensors, rng), rng),
-        NodeType::Command    => cross_over_command(a, b, choose(a_nodes.commands, rng), choose(b_nodes.commands, rng), rng)
+        NodeType::Program    => cross_over_program(a, b, choose(a_nodes.programs, rng), choose(b_nodes.programs, rng)),
+        NodeType::Expression => cross_over_expression(a, b, choose(a_nodes.expressions, rng), choose(b_nodes.expressions, rng)),
+        NodeType::Condition  => cross_over_condition(a, b, choose(a_nodes.conditions, rng), choose(b_nodes.conditions, rng)),
+        NodeType::Sensor     => cross_over_sensor(a, b, choose(a_nodes.sensors, rng), choose(b_nodes.sensors, rng)),
+        NodeType::Command    => cross_over_command(a, b, choose(a_nodes.commands, rng), choose(b_nodes.commands, rng))
     }
 }
 
-fn cross_over_program<'a, H: rand::Rng+Sized>(a: &Program, b: &Program, an: &Program, bn: &Program, rng: &mut H) -> (Box<Program>, Box<Program>) {
+fn cross_over_program(a: &Program, b: &Program, an: &Program, bn: &Program) -> (Box<Program>, Box<Program>) {
     (a.copy(&copy::CopyReplaceProgram { to_replace: an, replace_with: bn }),
      b.copy(&copy::CopyReplaceProgram { to_replace: bn, replace_with: an }))
 }
 
-fn cross_over_expression<'a, H: rand::Rng+Sized>(a: &Program, b: &Program, an: &Expression, bn: &Expression, rng: &mut H) -> (Box<Program>, Box<Program>) {
+fn cross_over_expression(a: &Program, b: &Program, an: &Expression, bn: &Expression) -> (Box<Program>, Box<Program>) {
     (a.copy(&copy::CopyReplaceExpression { to_replace: an, replace_with: bn }),
      b.copy(&copy::CopyReplaceExpression { to_replace: bn, replace_with: an }))
 }
 
-fn cross_over_condition<'a, H: rand::Rng+Sized>(a: &Program, b: &Program, an: &Condition, bn: &Condition, rng: &mut H) -> (Box<Program>, Box<Program>) {
+fn cross_over_condition(a: &Program, b: &Program, an: &Condition, bn: &Condition) -> (Box<Program>, Box<Program>) {
     (a.copy(&copy::CopyReplaceCondition { to_replace: an, replace_with: bn }),
      b.copy(&copy::CopyReplaceCondition { to_replace: bn, replace_with: an }))
 }
 
-fn cross_over_sensor<'a, H: rand::Rng+Sized>(a: &Program, b: &Program, an: &Sensor, bn: &Sensor, rng: &mut H) -> (Box<Program>, Box<Program>) {
+fn cross_over_sensor(a: &Program, b: &Program, an: &Sensor, bn: &Sensor) -> (Box<Program>, Box<Program>) {
     (a.copy(&copy::CopyReplaceSensor { to_replace: an, replace_with: bn }),
      b.copy(&copy::CopyReplaceSensor { to_replace: bn, replace_with: an }))
 }
 
-fn cross_over_command<'a, H: rand::Rng+Sized>(a: &Program, b: &Program, an: &Command, bn: &Command, rng: &mut H) -> (Box<Program>, Box<Program>) {
+fn cross_over_command(a: &Program, b: &Program, an: &Command, bn: &Command) -> (Box<Program>, Box<Program>) {
     (a.copy(&copy::CopyReplaceCommand { to_replace: an, replace_with: bn }),
      b.copy(&copy::CopyReplaceCommand { to_replace: bn, replace_with: an }))
 }
