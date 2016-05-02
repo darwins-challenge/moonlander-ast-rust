f5569d75a84307f05918b2135f939eae14cb1e2e
Daan van Berkel <daan@fifth-postulate.nl>
2016-05-02 20:41:15 +0000

    rebuild pages at b22625c

--- implementors/ast/random/trait.Rng.js
+++ implementors/ast/random/trait.Rng.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rand'] = [];implementors['ast'] = [];
+implementors['ast'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
