7ec74849047b04880b5ba44afce9a7c61d4a5908
Daan van Berkel <daan@fifth-postulate.nl>
2016-05-10 16:34:26 +0000

    rebuild pages at 1edd8d3

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rand'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rand/reseeding/struct.ReseedWithDefault.html' title='rand::reseeding::ReseedWithDefault'>ReseedWithDefault</a>",];
+implementors["rand"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rand/reseeding/struct.ReseedWithDefault.html' title='rand::reseeding::ReseedWithDefault'>ReseedWithDefault</a>",];implementors["ast"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rand/reseeding/struct.ReseedWithDefault.html' title='rand::reseeding::ReseedWithDefault'>ReseedWithDefault</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
