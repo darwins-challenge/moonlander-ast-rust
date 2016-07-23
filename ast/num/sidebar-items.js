2753e6937fed141f9b37ca22646bbd1b48201afc
Daan van Berkel <daan@fifth-postulate.nl>
2016-07-23 17:53:27 +0000

    rebuild pages at 0af96c9

--- ast/num/sidebar-items.js
+++ ast/num/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["angle_dist",""],["partial_max","A max() function that only requires a partial ordering."],["square",""]]});
+initSidebarItems({"constant":[["TAU",""]],"fn":[["angle_dist",""],["partial_max","A max() function that only requires a partial ordering."],["square",""]]});
