d810d85014881582a4df1c553f76488b8f1a918e
Daan van Berkel <daan@fifth-postulate.nl>
2016-10-26 19:16:35 +0000

    rebuild pages at 3929a4b

--- implementors/core/ops/trait.Div.js
+++ implementors/core/ops/trait.Div.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];implementors["moonlander_gp"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Div.html' title='core::ops::Div'>Div</a>&lt;<a class='type' href='moonlander_gp/num/type.Number.html' title='moonlander_gp::num::Number'>Number</a>&gt; for <a class='struct' href='moonlander_gp/struct.ScoreCard.html' title='moonlander_gp::ScoreCard'>ScoreCard</a>",];
+implementors["libc"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Div.html' title='core::ops::Div'>Div</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.u32.html'>u32</a>&gt; for <a class='struct' href='https://doc.rust-lang.org/nightly/std/time/duration/struct.Duration.html' title='std::time::duration::Duration'>Duration</a>",];implementors["moonlander_gp"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Div.html' title='core::ops::Div'>Div</a>&lt;<a class='type' href='moonlander_gp/num/type.Number.html' title='moonlander_gp::num::Number'>Number</a>&gt; for <a class='struct' href='moonlander_gp/struct.ScoreCard.html' title='moonlander_gp::ScoreCard'>ScoreCard</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
